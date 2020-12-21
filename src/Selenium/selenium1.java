14b532e0664821d04ff376c6d2baf82309de1089
umut2121 <umutyumruk@gmail.com>
2020-12-20 21:21:59 -0600

    Initial commit

---
+++ src/Selenium/selenium1.java
@@ -0,0 +1,111 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+public class selenium1 {
+    public static void main(String[] args) throws InterruptedException {
+
+
+//        System.setProperty("webdriver.chrome.driver","chromedriver");
+//        WebDriver driver=new ChromeDriver();
+//
+//        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+//
+//        TimeUnit.SECONDS.sleep(3);
+//
+//        WebElement noButton=driver.findElement(By.xpath("//a[contains(@id,'at-cv')]"));
+//        noButton.click();
+//
+//        System.out.println(driver.getTitle().equals("Selenium Easy Demo - Simple Form to Automate using Selenium"));
+//
+//
+//
+//        WebElement input=driver.findElement(By.id("user-message"));
+//        String a="First Test Case";
+//        input.sendKeys(a);
+//
+//        WebElement clickBut=driver.findElement(By.xpath("//button[@onclick='showInput();']"));
+//        clickBut.click();
+//
+//
+//        WebElement result=driver.findElement(By.xpath("//span[@id='display']"));
+//
+//       System.out.println(result.getText().equals(a));
+//
+/////////////////  TEST CASE 2 ////////////////
+
+//        System.setProperty("webdriver.chrome.driver","chromedriver");
+//        WebDriver driver=new ChromeDriver();
+//
+//        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+//
+//
+//        TimeUnit.SECONDS.sleep(3);
+//
+//        WebElement noButton=driver.findElement(By.xpath("//a[contains(@id,'at-cv')]"));
+//        noButton.click();
+//
+//        WebElement a=driver.findElement(By.id("sum1"));
+//        a.sendKeys("12");
+//
+//        WebElement b=driver.findElement(By.id("sum2"));
+//        b.sendKeys("17");
+//
+//        WebElement total=driver.findElement(By.xpath("//button[@onclick='return total()']"));
+//        total.click();
+//
+//        WebElement result=driver.findElement(By.id("displayvalue"));
+//
+
+//        System.out.println(result.getText().equals("29"));
+
+///////////////////TEST CASE 3/////////////////
+//
+//        System.setProperty("webdriver.chrome.driver","chromedriver");
+//        WebDriver driver=new ChromeDriver();
+//
+//        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+//
+//
+//        TimeUnit.SECONDS.sleep(3);
+//
+//        WebElement noButton=driver.findElement(By.xpath("//a[contains(@id,'at-cv')]"));
+//        noButton.click();
+//
+//        WebElement a=driver.findElement(By.id("sum1"));
+//        a.sendKeys("Chicago");
+//
+//        WebElement b=driver.findElement(By.id("sum2"));
+//        b.sendKeys("Illinois");
+//
+//        WebElement total=driver.findElement(By.xpath("//button[@onclick='return total()']"));
+//        total.click();
+//
+//        WebElement result=driver.findElement(By.id("displayvalue"));
+//
+//
+//        System.out.println(result.getText().equals("NaN"));
+
+//
+//        System.setProperty("webdriver.chrome.driver","chromedriver");
+//        WebDriver driver=new ChromeDriver();
+//
+//        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+//
+//        WebElement selectbx=driver.findElement(By.id("isAgeSelected"));
+//        selectbx.click();
+//
+//        WebElement result=driver.findElement(By.id("txtAge"));
+//        System.out.println(result.getText().equals("Success - Check box is checked"));
+
+
+
+
+    }
+}
